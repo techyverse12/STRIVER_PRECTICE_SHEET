35dc9efd66de89fd988144c037a3a5ac186e5044
Kavya goyal <kavaygoyal493@gmail.com>
2026-09-10 11:47:48 +0530

    Time: 46 ms (21.14%) | Memory: 34.7 MB (12.10%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,23 +3,29 @@ public:
     int countSubstrings(string s) {
         int n = s.size();
         int count = 0;
+        vector<vector<int>> memo(n, vector<int>(n, -1));
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
-                if(isPalindrome(s, i, j)) {
+                if(isPalindrome(s, i, j,memo)) {
                     count++;
                 }
             }
         }
         return count;
     }
-    bool isPalindrome(const string& s, int left, int right) {
-        while(left < right) {
-            if(s[left] != s[right]) {
-                return false;
-            }
-            left++;
-            right--;
+    bool isPalindrome(const string& s, int left, int right,vector<vector<int>>& memo) {
+        if(left>=right){
+            return 1;
         }
-        return true;
+        if(memo[left][right]!=-1){
+            return memo[left][right];
+        }
+        if(s[left]==s[right]){
+           memo[left][right] = isPalindrome(s, left + 1, right - 1, memo);
+        } 
+        else {
+            memo[left][right] = 0;
+        }
+        return memo[left][right];
     }
-};
+}; 
