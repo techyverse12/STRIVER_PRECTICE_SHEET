4163859befeb9e09ae831ec7bd4e5e9d5f7f2ce2
Kavya goyal <kavaygoyal493@gmail.com>
2026-09-10 11:33:26 +0530

    Time: 370 ms (18.74%) | Memory: 8.5 MB (91.39%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(isPalindrome(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+private:
+    bool isPalindrome(const string& s, int left, int right) {
+        while(left < right) {
+            if(s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
