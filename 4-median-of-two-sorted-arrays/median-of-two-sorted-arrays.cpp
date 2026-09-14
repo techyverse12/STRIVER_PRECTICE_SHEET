ab0908be97ea417465a4cd4086af2241fd67d892
Kavya goyal <kavaygoyal493@gmail.com>
2026-09-14 15:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (79.76%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0;
-        int j=0;
-        int total=nums1.size()+nums2.size();
-        int counter=total/2;
-        int prev=0;
-        int curr=0;
-        int timer=0;
-            while(i<nums1.size() && j<nums2.size() && timer<=counter){
-                prev=curr;
-                if(nums1[i]<nums2[j]){
-                    curr=nums1[i];
-                    i++;
-                }
-                else{
-                    curr=nums2[j];
-                    j++;
-                }
-                timer++;
-            }
-            while(i<nums1.size() && timer<=counter){
-                prev=curr;
-                curr=nums1[i];
-                i++;
-                timer++;
-            }
-            while(j<nums2.size() && timer<=counter){
-                prev=curr;
-                curr=nums2[j];
-                j++;
-                timer++;
-            }
-            if(total % 2==1){
-                return curr;
+        if(nums1.size()>nums2.size()){
+           return  findMedianSortedArrays(nums2,nums1);
+        }
+        int n=nums1.size();
+        int m=nums2.size();
+        int left=(n+m+1)/2;
+        int low=0;
+        int high=n;
+        while(low<=high){
+            int i=(low+high)/2;
+            int j=left-i;
+            int l1,l2,r1,r2;
+            if(i==0){
+                l1=INT_MIN;
+             }
+             else{
+                l1=nums1[i-1];
+             }
+            if(i==n){
+                r1=INT_MAX;
             }
             else{
-                return (prev+curr)/2.0;
+                 r1=nums1[i];
             }
+            if(j==0){
+                l2=INT_MIN;
+            }
+            else{
+                l2=nums2[j-1];
+            }
+            if(j==m){
+                r2=INT_MAX;
+            }
+            else{
+                r2=nums2[j];
+            }
+            if(l1>r2){
+                high=i-1;
+            }
+            else if(l2>r1){
+                low=i+1;
+            }
+            else if(l2<=r1 && l1<=r2){
+                if((n+m)%2==1){
+                    return max(l1,l2);
+                }
+                else{
+                   return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+            }
+        }
+        return 0.0;
     }
 };
